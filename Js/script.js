a55d666671d8efbc3a55bead9351c73e0139b72d
Keldiyor1303 <keldiyorraxmatov1303@gmail.com>
2022-01-30 22:59:14 +0500

    Savatga qo`shildi

--- Js/script.js
+++ Js/script.js
@@ -5,7 +5,7 @@ function chiqar(maxsulot) {
     section.innerHTML = ""
 
     allBtn.forEach((btn) => {
-        btn.style = "background-color: white; color: #E96023; border: 1px solid #E96023"
+        btn.style = "background-color: white; color: #E96023; border: 2px solid #E96023"
     })
 
     maxsulot.forEach((element) => {
@@ -18,7 +18,7 @@ function chiqar(maxsulot) {
                 <div class="info-div">
                     <h2>${element.nom}</h2>
                     <p>${element.narx} so\`m </p>
-                    <button>Savatga qo\`shish</button>
+                    <button data-savat-id="${element.id}" id="savat">Savatga qo\`shish</button>
                 </div>
             `
 
@@ -37,34 +37,135 @@ const IchimliklarBtn = document.querySelector("#Ichimliklar")
 
 function Barchasi() {
     chiqar(maxsulotlar)
-    BarchasiBtn.style = "background-color: #FADF00; color: white; border: 1px solid transparent;"
-
+    BarchasiBtn.style = "background-color: #FADF00; color: white; border: 2px solid transparent;"
+    savatgaQosh()
 }
 
 Barchasi()
 
-
-
 function Sabzavotlar() {
     const maxsulot = maxsulotlar.filter((element) => element.tur == "Sabzovot")
     chiqar(maxsulot)
-    SabzavotlarBtn.style = "background-color: #FADF00; color: white; border: 1px solid transparent;"
+    SabzavotlarBtn.style = "background-color: #FADF00; color: white; border: 2px solid transparent;"
+    savatgaQosh()
+
 }
 
 function Mevalar() {
     const maxsulot = maxsulotlar.filter((element) => element.tur == "Meva")
     chiqar(maxsulot)
-    MevalarBtn.style = "background-color: #FADF00; color: white; border: 1px solid transparent;"
+    MevalarBtn.style = "background-color: #FADF00; color: white; border: 2px solid transparent;"
+    savatgaQosh()
+
 }
 
 function Parranda() {
     const maxsulot = maxsulotlar.filter((element) => element.tur == "Go`sht va parranda")
     chiqar(maxsulot)
-    ParrandaBtn.style = "background-color: #FADF00; color: white; border: 1px solid transparent;"
+    ParrandaBtn.style = "background-color: #FADF00; color: white; border: 2px solid transparent;"
+    savatgaQosh()
+
 }
 
 function Ichimliklar() {
     const maxsulot = maxsulotlar.filter((element) => element.tur == "Ichimlik")
     chiqar(maxsulot)
-    IchimliklarBtn.style = "background-color: #FADF00; color: white; border: 1px solid transparent;"
+    IchimliklarBtn.style = "background-color: #FADF00; color: white; border: 2px solid transparent;"
+    savatgaQosh()
+
 }
+
+
+// SAVATGA QO`SHISH
+function savatgaQosh() {
+    const savatBtn = document.querySelectorAll("#savat")
+
+    for (let savat of savatBtn) {
+
+        savat.addEventListener("click", function () {
+            const { savatId } = savat.dataset
+
+            savatgajonat(savatId)
+        })
+    }
+}
+
+let savat = [];
+
+let borYokiYoq = 0
+
+function savatgajonat(savatId) {
+
+    maxsulotlar.forEach((element) => {
+        if (element.id == savatId) {
+            borYokiYoq = 0
+
+            savat.forEach((element2) => {
+                if (element2.id == element.id) {
+                    element2.soni = element2.soni + 1;
+                    borYokiYoq++
+                }
+            })
+
+            if (borYokiYoq == 0) {
+                savat.push({
+                    id: element.id,
+                    savatId: savat[savat.length - 1]?.savatId + 1 || 1,
+                    soni: 1,
+                    tur: element.tur,
+                    narx: element.narx,
+                    nom: element.nom,
+                })
+            }
+
+            savatgachiqar(savat)
+        }
+    })
+
+}
+
+const chiqarish = document.querySelector(".savat")
+
+function savatgachiqar(savat) {
+
+    chiqarish.innerHTML = ""
+
+    savat.forEach((element) => {
+        const div = document.createElement("div")
+
+        div.innerHTML = `
+            <div class="info-1">
+            <p>${element.nom}</p>
+            <p>${element.soni}</p>
+            </div>
+
+            <div class="info-2">
+            <p>Summa: ${element.soni * element.narx} so\`m</p>
+            <button data-ochirish-id = ${element.id} id="ochirish" >O\`chirish</button>
+    </div>        
+        `
+
+        chiqarish.appendChild(div)
+
+    })
+}
+
+
+function ochirish() {
+    const ochirishbtns = document.querySelectorAll("#ochirish")
+
+    ochirishbtns.forEach((ochirishbtn) => {
+
+        ochirishbtn.addEventListener("click", function () {
+            const { ochirishId } = ochirishbtn.dataset
+            console.log(1)
+
+            savat = savat.filter((element) => element.id != ochirishId)
+            savatgachiqar(savat)
+        })
+
+    })
+
+}
+
+ochirish()
